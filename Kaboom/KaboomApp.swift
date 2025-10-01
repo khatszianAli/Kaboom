c97863a41d212fbbc0cf554be2eb63d428ae7df6
ka12438 <ka12438@auca.kg>
2025-10-01 15:51:28 +0600

    Initial Commit

---
+++ Kaboom/KaboomApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaboomApp.swift
+//  Kaboom
+//
+//  Created by new owner on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct KaboomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KaboomTests/KaboomTests.swift
@@ -0,0 +1,17 @@
+//
+//  KaboomTests.swift
+//  KaboomTests
+//
+//  Created by new owner on 1/10/25.
+//
+
+import Testing
+@testable import Kaboom
+
+struct KaboomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
